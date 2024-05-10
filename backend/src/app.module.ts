a1e321d41fbf3d016972dfec25b7faf89a3fb136
JhonnK08 <38366068+JhonnK08@users.noreply.github.com>
2024-05-09 23:02:47 -0300

    refactor(Backend): removed unused app files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [TaskModule, ConfigModule.forRoot()],
+	controllers: [],
+	providers: []
 })
 export class AppModule {}
