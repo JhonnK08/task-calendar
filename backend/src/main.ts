e2caf65080a89d444cd1edbcc2e44e2e10b902b3
Jhonatan Konopp <jhonnkonopp@gmail.com>
2024-05-10 23:54:34 -0300

    feat(Backend): added swagger configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	const config = new DocumentBuilder()
+		.setTitle('Task calendar')
+		.setDescription('Task calendar API documentation.')
+		.setVersion('0.0.1')
+		.addTag('task')
+		.addTag('tag')
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('docs', app, document);
 
 	app.useGlobalPipes(
 		new ValidationPipe({
@@ -12,5 +23,6 @@ async function bootstrap() {
 	);
 
 	await app.listen(3000);
+	console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
