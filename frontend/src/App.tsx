cddd51c60030f3a1fcb80fab8888bbe7154e2b45
Jhonatan Konopp <jhonnkonopp@gmail.com>
2024-05-12 19:56:32 -0300

    feat(Frontend): added Dashboard and provider to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from './components/Header/Header';
+import { ThemeProvider } from './components/ThemeProvider/ThemeProvider';
+import { TooltipProvider } from './components/ui/Tooltip';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+			<TooltipProvider delayDuration={300}>
+				<div className='flex h-screen w-screen flex-col items-start justify-center bg-background text-foreground'>
+					<Header />
+					<div className='flex w-screen flex-1 flex-col items-center justify-start gap-y-4 overflow-hidden p-4'>
+						<Dashboard />
+					</div>
+				</div>
+			</TooltipProvider>
+		</ThemeProvider>
+	);
 }
 
-export default App
+export default App;
