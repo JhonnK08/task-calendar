05eda399d2f534afd8095d3571239b08d9aa3670
Jhonatan Konopp <jhonnkonopp@gmail.com>
2024-05-12 21:44:26 -0300

    feat(Frontend/Components): added new task button on header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,11 +1,15 @@
 import { ReactElement } from 'react';
+import NewTaskButton from '../NewTaskButton/NewTaskButton';
 import { ToggleThemeMode } from '../ToggleThemeMode/ToggleThemeMode';
 
 function Header(): ReactElement {
 	return (
 		<header className='flex h-14 w-full items-center justify-between border-b bg-background p-4'>
 			<h1 className='text-lg font-semibold'>Task Calendar</h1>
-			<ToggleThemeMode />
+			<div className='flex gap-x-4'>
+				<NewTaskButton />
+				<ToggleThemeMode />
+			</div>
 		</header>
 	);
 }
