0446cab7583b2aa66f2c8b51f9a90891cee2bcae
Jhonatan Konopp <jhonnkonopp@gmail.com>
2024-05-13 00:17:16 -0300

    feat(Frontend/Components): created SearchFormInput

--- frontend/src/components/Input/SearchFormInput.tsx
+++ frontend/src/components/Input/SearchFormInput.tsx
@@ -1,19 +1,32 @@
 import { Search } from 'lucide-react';
 import { ReactElement } from 'react';
+import { Control, FieldValues, Path } from 'react-hook-form';
 import { cn } from 'src/utils';
-import { Input } from '../ui/Input';
+import FormInput from './FormInput';
 
-interface SearchFormInputProperties {
+interface SearchFormInputProperties<TFieldValues extends FieldValues> {
+	name: Path<TFieldValues>;
+	control: Control<TFieldValues>;
 	className?: string;
+	onChangeSearchValue: (value: string) => void;
 }
 
-function SearchFormInput({
-	className
-}: SearchFormInputProperties): ReactElement {
+function SearchFormInput<TFieldValues extends FieldValues>({
+	className,
+	control,
+	name,
+	onChangeSearchValue
+}: SearchFormInputProperties<TFieldValues>): ReactElement {
 	return (
-		<div className={cn('relative w-full', className)}>
+		<div className={cn('relative w-full flex-1', className)}>
 			<Search className='absolute left-2 top-3 h-4 w-4' />
-			<Input placeholder='Search' className='pl-8' />
+			<FormInput
+				placeholder='Search'
+				className='pl-8'
+				control={control}
+				name={name}
+				onValueChange={onChangeSearchValue}
+			/>
 		</div>
 	);
 }
